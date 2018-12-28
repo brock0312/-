52d3436d4974ef01a3e1d85f36976c184a0dd323
mikemikemike451 <46022223+mikemikemike451@users.noreply.github.com>
2018-12-28 20:25:17 +0800

    mikemikemike
    only datetime and format undone

--- 期末爬蟲 拷貝.py
+++ 期末爬蟲 拷貝.py
@@ -1,45 +1,76 @@
-#東南亞
-import requests
-requests.get('https://movies.yahoo.com.tw/theater_result.html/id=53')
-res = requests.get('https://movies.yahoo.com.tw/theater_result.html/id=53')
-res.text
+satis = dict()
+def fuction(name,url):
+	import requests
+	requests.get(url)
+	res = requests.get(url)
+	res.text
 
-from bs4 import BeautifulSoup
-Soup = BeautifulSoup(res.text,'html.parser')
+	from bs4 import BeautifulSoup
+	Soup = BeautifulSoup(res.text,'html.parser')
 
-tag_name = 'div.theaterlist_name a'
-movie_time = 'ul.theater_time  '
-articles = Soup.select(tag_name)
-article2 = Soup.select(movie_time)
+	tag_name = 'div.theaterlist_name a'
+	movie_time = 'ul.theater_time  '
+	star = 'div.leveltext.starwithnum span.count'
+	articles = Soup.select(tag_name)
+	article2 = Soup.select(movie_time)
+	article3 = Soup.select(star)
 
+#電影名稱	
+	影城movie = []
+	for art in articles:
+		art = str(art)
+		num1 = art.find('>')
+		art = art[num1+1:-4]
+		影城movie.append(art)
+#電影場次
+	影城timecode = []
+	影城time = []
+	find_time1=0
 
-東南亞movie = []
-for art in articles:
-	art = str(art)
-	num1 = art.find('>')
-	art = art[num1+1:-4]
-	東南亞movie.append(art)
+	for art in article2:
+		art = str(art)
+		num2 = art.find('">')
+		art = art[num2+2:-5]
+		影城timecode.append(art)
+		
+	for i in 影城timecode:
+		n=0
+		ava_time=[]
+		影城time.append(ava_time)
+		for j in i:
+			n+=1
+			if j == 't':
+				find_time1=i.find('t">',n-1)
+				ava_time.append(i[find_time1+3:find_time1+8])
 
-東南亞long = []
-東南亞time = []
-find_time1=0
+#滿意度
+	
+	score=[]
+	for art in article3:
+		art = str(art)
+		num3 = art.find('">')
+		num4 = art.find('</')
+		art = art[num3+2:num4]
+		art= float(art)
+		score.append(art)
+	for i in range (len(影城movie)):
+		satis[影城movie[i]]=score[i]
+		
+#輸出格式
+	for i in 影城time:
+		if len(i)==0:
+			i.append('今日已無場次')
+	print(name)
+	for a in range(len(影城movie)):
+		timelist = " ".join(str(e) for e in 影城time[a])
+		print("@"+str(影城movie[a]))
+		print("    "+"場次: "+timelist)
+	print()
+fuction("東南亞秀泰影城",'https://movies.yahoo.com.tw/theater_result.html/id=53')
+fuction("百老匯數位影城",'https://movies.yahoo.com.tw/theater_result.html/id=52')
+fuction("梅花數位影院",'https://movies.yahoo.com.tw/theater_result.html/id=126')	
 
-for art in article2:
-	art = str(art)
-	num2 = art.find('">')
-	art = art[num2+2:-5]
-	東南亞long.append(art)
-for i in 東南亞long:
-	n=0
-	ava_time=[]
-	東南亞time.append(ava_time)
-	for j in i:
-		n+=1
-		if j == 't':
-			find_time1=i.find('t">',n-1)
-			ava_time.append(i[find_time1+3:find_time1+8])
-for i in 東南亞time:
-	if len(i)==0:
-		i.append('今日已無場次')
-for a in range(len(東南亞movie)):
-	print(東南亞movie[a],東南亞time[a])
+scoreorder = sorted( [ [-satis[key],key] for key in satis ] )  #用前者VALUE排序
+for i in range(len(satis)):
+	k = scoreorder[i][1]
+	print(k,-scoreorder[i][0])
